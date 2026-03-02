68c0557706ae55718771ad82a9524309f1c48902
yassenmmdouh <yaya12342020@gmail.com>
2026-03-03 01:52:14 +0200

    Add files via upload

---
+++ js/store.js
@@ -0,0 +1,102 @@
+let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+function saveCart(){
+  localStorage.setItem("cart", JSON.stringify(cart));
+  updateCartCount();
+}
+
+function updateCartCount(){
+  const count = cart.reduce((a,b)=>a+b.qty,0);
+  const el = document.getElementById("cart-count");
+  if(el) el.textContent = count;
+}
+
+function addToCart(name, price, img){
+
+  const existing = cart.find(p=>p.name===name);
+
+  if(existing){
+    existing.qty++;
+  }else{
+    cart.push({
+      name,
+      price,
+      img,
+      qty:1
+    });
+  }
+
+  saveCart();
+  renderCart();
+}
+
+function renderCart(){
+  const container = document.getElementById("cart-items");
+  const totalEl = document.getElementById("cart-total");
+  if(!container) return;
+
+  container.innerHTML="";
+  let total = 0;
+
+  cart.forEach((item,i)=>{
+    total += item.price * item.qty;
+
+ container.innerHTML += `
+<div class="cart-item">
+
+  <img src="${item.img}" alt="${item.name}">
+
+  <div class="cart-info">
+    <h4>${item.name}</h4>
+    <p>${item.price} EGP</p>
+    <p>Qty: ${item.qty}</p>
+  </div>
+
+  <button onclick="removeItem(${i})">×</button>
+
+</div>
+`;
+  });
+
+  if(totalEl) totalEl.textContent = "Total: "+total+" EGP";
+}
+
+function removeItem(i){
+  cart.splice(i,1);
+  saveCart();
+  renderCart();
+}
+
+function toggleCart(){
+  document
+    .getElementById("cart-popup")
+    .classList.toggle("active");
+
+  renderCart();
+}
+
+function goToCheckout(){
+  window.location.href="checkout.html";
+}
+/* CLOSE CART WHEN CLICK OUTSIDE */
+
+document.addEventListener("click", function(e){
+
+  const cart = document.getElementById("cart-popup");
+  const cartBtn = e.target.closest("[onclick='toggleCart()']");
+
+  if(!cart) return;
+
+  if(
+    cart.classList.contains("active") &&
+    !cart.contains(e.target) &&
+    !cartBtn
+  ){
+    cart.classList.remove("active");
+  }
+
+});
+updateCartCount();
+window.toggleCart = toggleCart;
+window.removeItem = removeItem;
+window.goToCheckout = goToCheckout;
